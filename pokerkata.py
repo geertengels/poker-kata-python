fcac1907d019152773473fc5333495c7c86e5383
geertengels <geert.engels@gmail.com>
2017-12-14 15:50:54 +0000

    Missing space in comment

--- pokerkata.py
+++ pokerkata.py
@@ -286,7 +286,7 @@ if __name__ == '__main__':
 
       # Two of a Kind
       ("Black: 2D 3D 4D 5D 7D  White: 2C 3C 4C 5C 7C", "Not Ranked"), # no pairs
-      ("Black: 2D 2C 4D 5D 6D  White: 2H 3C 4C 5C 7C", "Win Black, Two of a Kind"), #black has only pair
+      ("Black: 2D 2C 4D 5D 6D  White: 2H 3C 4C 5C 7C", "Win Black, Two of a Kind"), # black has only pair
       ("Black: 2D 3D 4D 5D 7H  White: 2C 2H 4C 5C 6C", "Win White, Two of a Kind"), # white has only pair
       ("Black: 2D 2S 4D 5D 6D  White: 2C 2H 4C 5C 6C", "Draw"), # both have a pair, with same face value, and all remaining cards also match by face value
       ("Black: 2D 2C 4D 5D 6D  White: 3C 3H 4C 5C 6C", "Win White, Two of a Kind: 3 over 2"), # both have a pair, but white higher face
